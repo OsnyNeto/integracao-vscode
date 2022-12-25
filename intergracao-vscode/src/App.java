f1b4a2b469a9ebbcebc683268225c0f922597921
OsnyNeto <osnynt@gmail.com>
2022-12-25 05:10:30 -0300

    build dando erro

---
+++ intergracao-vscode/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, osny!");
+
+        Integer numero = 20;
+
+        System.out.println(numero);
+    }
+}
